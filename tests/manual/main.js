ae868f5e2f5278a658f22c28da5b6710ead0c4c8
QuantomErik <ey222ci@student.lnu.se>
2024-09-18 13:37:03 +0200

    Work on manual test, defining passwordResultDiv and emailResultDiv directly in HTML.

--- tests/manual/main.js
+++ tests/manual/main.js
@@ -3,7 +3,14 @@ import { PasswordValidator } from '../../src/PasswordValidator.js'
 
 
 const form = document.getElementById('form')
-const resultDiv = document.getElementById('result')
+/* const resultDiv = document.getElementById('result') */
+
+/* const passwordResultDiv = document.createElement('div') */
+const passwordResultDiv = document.getElementById('passwordResultDiv')
+/* const emailResultDiv = document.createElement('div') */
+const emailResultDiv = document.getElementById('emailResultDiv')
+/* resultDiv.appendChild(passwordResultDiv)
+resultDiv.appendChild(emailResultDiv) */
 
 const passwordValidator = new PasswordValidator()
 passwordValidator.addRule(passwordValidator.hasDangerousChars)
@@ -14,17 +21,44 @@ emailValidator.addRule(emailValidator.hasAtSymbol)
 form.addEventListener('submit', function(event) {
   event.preventDefault()
 
-  const password = document.getElementById('password').value
-  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value.trim()
+  const email = document.getElementById('email').value.trim()
+
+  //Clear previous validation results
+  passwordResultDiv.textContent = ''
+  emailResultDiv.textContent = ''
+  passwordResultDiv.className = ''
+  emailResultDiv.className = ''
+
+  if (password !== '') {
+    const passwordResult = passwordValidator.validate(password)
+    passwordResultDiv.textContent = passwordResult
+    if (passwordResult === 'Password is valid.') {
+      passwordResultDiv.className = 'success'
+    } else {
+      passwordResultDiv.className = 'error'
+    }
+  }
+
+  if (email !== '') {
+    const emailResult = emailValidator.validate(email)
+    emailResultDiv.textContent = emailResult
+    if (emailResult === 'Email is valid.') {
+      emailResultDiv.className = 'success'
+    } else {
+      emailResultDiv.className = 'error'
+    }
+  }
 
   
-  const passwordResult = passwordValidator.validate(password)
+
+  /* const passwordResult = passwordValidator.validate(password) */
 
  
-  const emailResult = emailValidator.validate(email)
+  /* const emailResult = emailValidator.validate(email) */
 
 
-  if (passwordResult === 'Password is valid.') {
+ /*  if (passwordResult === 'Password is valid.') {
     resultDiv.textContent = passwordResult
     resultDiv.className = 'success'
   } else {
@@ -38,5 +72,5 @@ form.addEventListener('submit', function(event) {
   } else {
     resultDiv.textContent = emailResult
     resultDiv.className = 'error'
-  }
+  } */
 })
