b0676291e3137eeea3082c450336dd7b70b74768
QuantomErik <ey222ci@student.lnu.se>
2024-10-23 13:20:02 +0200

    test.

---
+++ L3/dist/assets/homepage-BNSzsagY.js
@@ -0,0 +1,1 @@
+document.getElementById("registerButton").addEventListener("click",function(){window.location.href="/src/registerform/registerform.html"});document.getElementById("loginButton").addEventListener("click",function(){window.location.href="/src/login/login.html"});
--- L3/vite.config.js
+++ L3/vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       input: {
-        /* main: './index.html', */
-        main: './src/homepage/homepage.html',
-       /*  homepage: './src/homepage/homepage.html', */
+        main: './index.html',
+       /*  main: './src/homepage/homepage.html', */
+        homepage: './src/homepage/homepage.html',
         register: './src/registerform/registerform.html',
         login: './src/login/login.html',
         userpage: './src/userpage/userpage.html',  // Add all your component HTML files
