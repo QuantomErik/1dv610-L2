77adeb01f1aa3440e66062054257d53fa44b3b64
QuantomErik <ey222ci@student.lnu.se>
2024-10-23 12:55:34 +0200

    test.

--- L3/dist/assets/homepage-BNSzsagY.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("registerButton").addEventListener("click",function(){window.location.href="/src/registerform/registerform.html"});document.getElementById("loginButton").addEventListener("click",function(){window.location.href="/src/login/login.html"});
