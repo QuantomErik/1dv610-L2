9baa94b9235045e3967b420f8fadae3b58bb512f
QuantomErik <ey222ci@student.lnu.se>
2024-10-24 15:03:40 +0200

    add vite.config.js.

--- L3/vite.config.js
+++ L3/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/* import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: './',
@@ -7,13 +7,27 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './index.html',
-       /*  main: './src/homepage/homepage.html', */
+       
         homepage: './src/homepage/homepage.html',
         register: './src/registerform/registerform.html',
         login: './src/login/login.html',
-        userpage: './src/userpage/userpage.html',  // Add all your component HTML files
+        userpage: './src/userpage/userpage.html',
       },
     },
   },
   publicDir: 'module-L2',
+})
+ */
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  
+  base: './',
+
+  
+  build: {
+    outDir: 'dist', 
+  },
+
 })
